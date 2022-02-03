58665e10ef24216dd46f99439d3f2f6bbf0947e4
Taylor Reiter <taylorreiter@gmail.com>
2022-02-03 09:37:31 -0800

    address review suggestions

--- scripts/featureCounts_se.R
+++ scripts/featureCounts_se.R
@@ -1,3 +1,22 @@
+# featureCounts is a function in the Rsubread package used to estimate the number of
+# reads that overlap with a given feature in a genome from a BAM file of reads that
+# have been mapped to that reference. The featureCounts() function is highly 
+# parameterizable, which gives it a lot of flexibility for different sequencing 
+# technologies, but it also makes it difficult to know whether the exact set of 
+# correct parameters is always being used. 
+# As invoked here, featureCounts:
+#     1. requires a minimum overlap of 31 base pairs between a read and a feature.
+#        This was somewhat arbitrary, but I selected it because it matches the 
+#        default minimum k-mer overlap for a read and a transcript in salmon.
+#     2. Counts at the CDS level.
+#     3. Allows reads to overlap multiple features. This allows "spanning" reads 
+#        that likely correspond to reads that overlap two genes, as occurs in 
+#        polycistronic transcripts from multi-gene operons.
+#     4. Counts reads fractionally if they overlap multiple features. 
+#     5. Ignores strandedness
+#     6. Counts as SE (reads were aligned to the reference as SE regardless of 
+#        whether they were SE or PE.
+
 library(Rsubread)
 
 counts <-featureCounts(files=snakemake@input[['bam']],
